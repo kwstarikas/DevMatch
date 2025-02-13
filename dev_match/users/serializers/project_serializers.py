6c29dd9b08efba52781249d7abe206897ea60774
Loukas Merkouris <loukasmerkouris@gmail.com>
2025-02-13 20:25:05 +0200

    Add serializers for Project and refactor code
    - Change the structure, add files for project and user serializers

---
+++ dev_match/users/serializers/project_serializers.py
@@ -0,0 +1,196 @@
+import pprint
+
+from django.db import transaction
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+from ..models import Developer, Skill, Project
+from .user_serializers import UserSerializer, SkillSerializer
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+class CollaboratorsSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(source="developer.skills", many=True)
+    username = serializers.CharField()
+    email = serializers.CharField()
+
+    class Meta:
+        model = Developer
+        fields = [
+            "username",
+            "email",
+            "skills",
+        ]
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    collaborators = CollaboratorsSerializer(many=True, read_only=True)
+    apllied_collaborators = CollaboratorsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "owner",
+            "project_name",
+            "description",
+            "maximum_collaborators",
+            "status",
+            "open_positions",
+            "collaborators",
+            "apllied_collaborators",
+        ]
+
+
+class CreateProjectSerializer(serializers.ModelSerializer):
+    collaborators = CollaboratorsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "owner",
+            "project_name",
+            "description",
+            "maximum_collaborators",
+            "status",
+            "open_positions",
+            "collaborators",
+        ]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "owner": {"read_only": True},
+            "status": {"read_only": True},
+            "open_positions": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        print("In validate")
+        return attrs
+
+    def create(self, validated_data):
+        print("In create")
+        user = self.context.get("user")
+        validated_data["status"] = "RN"
+        validated_data["open_positions"] = validated_data["maximum_collaborators"]
+        Project.objects.create(**validated_data, owner=user)
+        return validated_data
+
+
+class ApplyToProjectSerializer(serializers.ModelSerializer):
+    collaborators = CollaboratorsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = [
+            "project_name",
+            "collaborators",
+            "open_positions",
+            "maximum_collaborators",
+        ]
+        extra_kwargs = {
+            "project_name": {"read_only": True},
+            "open_positions": {"read_only": True},
+            "maximum_collaborators": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        print("in Validate")
+        username = self.context.get("username")
+        if not User.objects.filter(username=username).exists():
+            raise serializers.ValidationError(
+                f"Invalid username {username} does not exist"
+            )
+
+        if not self.instance.open_positions > 0:
+            raise serializers.ValidationError(
+                f"This project {self.instance} does not have open positions "
+            )
+        if self.instance.status != "RN":
+            raise serializers.ValidationError(
+                f"This project {self.instance} is not running so it doesnt accept any more contributions"
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        instance.apllied_collaborators.add(
+            User.objects.get(username=self.context.get("username"))
+        )
+
+        return instance
+
+
+class AcceptContriburtor(ApplyToProjectSerializer):
+    username = serializers.CharField()
+
+    class Meta:
+        model = Project
+        fields = ApplyToProjectSerializer.Meta.fields + ["username"]
+        extra_kwargs = {
+            "project_name": {"read_only": True},
+            "open_positions": {"read_only": True},
+            "maximum_collaborators": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        user_id = self.context.get("user_id")
+        if not User.objects.filter(id=user_id).exists():
+            raise serializers.ValidationError(
+                f"Invalid user with id  {user_id} does not exist"
+            )
+
+        if str(self.instance.owner.id) != user_id:
+            raise serializers.ValidationError(
+                "You dont have permision do accept in project you are not owner"
+            )
+        if not self.instance.check_collaborator_exists(attrs["username"]):
+            raise serializers.ValidationError(
+                "invalid username, does not exist in applied collaborators"
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            instance.move_from_applied_to_collaborators(validated_data["username"])
+            instance.save()
+
+        return validated_data
+
+
+class DeclineContriburtor(ApplyToProjectSerializer):
+    username = serializers.CharField()
+
+    class Meta:
+        model = Project
+        fields = ApplyToProjectSerializer.Meta.fields + ["username"]
+        extra_kwargs = {
+            "project_name": {"read_only": True},
+            "open_positions": {"read_only": True},
+            "maximum_collaborators": {"read_only": True},
+        }
+
+    def validate(self, attrs):
+        user_id = self.context.get("user_id")
+        if not User.objects.filter(id=user_id).exists():
+            raise serializers.ValidationError(
+                f"Invalid user with id  {user_id} does not exist"
+            )
+
+        if str(self.instance.owner.id) != user_id:
+            raise serializers.ValidationError(
+                "You dont have permision do decline in project you are not owner"
+            )
+        if not self.instance.check_collaborator_exists(attrs["username"]):
+            raise serializers.ValidationError(
+                "invalid username, does not exist in applied collaborators"
+            )
+        return attrs
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            instance.decline_applied_collaborators(validated_data["username"])
+            instance.save()
+
+        return validated_data
---
+++ dev_match/users/serializers/user_serializers.py
@@ -0,0 +1,182 @@
+import pprint
+
+from django.db import transaction
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
+from ..models import Developer, Skill
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ["level", "language"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(source="developer.age")
+    country = serializers.CharField(source="developer.country")
+    residence = serializers.CharField(source="developer.residence")
+    available_skills = SkillSerializer(source="developer.skills", many=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "age",
+            "country",
+            "residence",
+            "available_skills",
+        ]
+
+
+class ProgrammingLanguageSerializer(serializers.Serializer):
+    level = serializers.CharField(required=False)
+    name = serializers.CharField()
+
+    class Meta:
+        fields = ["name", "level"]
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    age = serializers.IntegerField(source="developer.age")
+    country = serializers.CharField(source="developer.country")
+    residence = serializers.CharField(source="developer.residence")
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "age",
+            "country",
+            "residence",
+        ]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "password": {"write_only": True},
+        }
+
+    def validate_age(self, value):
+        if value < 14:
+            raise serializers.ValidationError("Too young to join the platform")
+        return value
+
+    def validate(self, attrs):
+        if User.objects.filter(email=attrs["email"].lower()):
+            raise serializers.ValidationError("This email address already exists")
+        return attrs
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            developer_data = validated_data.pop("developer")
+            user = User.objects.create(**validated_data)
+            Developer.objects.create(user=user, **developer_data)
+            return user
+        raise serializers.ValidationError(
+            "Error While creating the User, plz try again xd "
+        )
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        field = ["password"]
+
+    def validate(self, data):
+        password = data.get("password")
+        token = self.context.get("token", None)
+        user_id = self.context.get("user_id", None)
+
+        if token is None or user_id is None:
+            raise serializers.ValidationError("Error invalid url")
+
+        try:
+            user = User.objects.get(pk=user_id)
+        except:
+            raise serializers.ValidationError("User ID is invalid")
+        if not PasswordResetTokenGenerator().check_token(user, token):
+            raise serializers.ValidationError("The reset token is invalid")
+
+        user.set_password(password)
+        user.save()
+        return data
+
+
+class RequestPasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class AddSkillSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer()
+
+    class Meta:
+        model = User
+        fields = ["skills"]
+
+    def validate(self, attrs):
+        dev = Developer.objects.get(user=self.instance)
+
+        if dev.skills.all().count() >= 3:
+            raise serializers.ValidationError(
+                "You already have 3 skills, if you want to add, delete one first"
+            )
+        if dev.skills.filter(language=attrs["skills"]["language"]).exists():
+            raise serializers.ValidationError("language of this skill already exists")
+
+        print("AVAILABLE OPTIONS : \n", dict(Skill.ProgrammingLanguage.choices))
+        return attrs
+
+    def update(self, instance, validated_data):
+        with transaction.atomic():
+            skill_to_add = Skill.objects.get_or_create(
+                level=validated_data["skills"]["level"],
+                language=validated_data["skills"]["language"],
+            )
+            dev = Developer.objects.get(user=instance)
+            dev.skills.add(Skill.objects.get(id=skill_to_add[0].id))
+            dev.save()
+
+        return validated_data
+
+
+class RemoveSkillSerializer(AddSkillSerializer):
+    class Meta:
+        model = User
+        fields = AddSkillSerializer.Meta.fields
+
+    def validate(self, attrs):
+        dev = Developer.objects.get(user=self.instance)
+
+        try:
+            exist = dev.skills.get(language=attrs["skills"]["language"])
+            print(exist)
+        except Exception as e:
+            print(e)
+            raise serializers.ValidationError("User does not have this skill")
+
+        return attrs
+
+    def update(self, instance, validated_data):
+        dev = Developer.objects.get(user=instance)
+        dev.skills.remove(
+            Skill.objects.get(
+                level=validated_data["skills"]["level"],
+                language=validated_data["skills"]["language"],
+            )
+        )
+        dev.save()
+
+        return validated_data
